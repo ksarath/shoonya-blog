cb83a3dfa3755472d0ee246c3916c2dc82297265
Sarath Kallatt Sivasankaran <kssarath2005@gmail.com>
2020-02-15 23:54:15 +0100

    organisation name corrected

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -27,7 +27,7 @@ const siteConfig = {
   baseUrl: '/shoonya-blog/',
 
   projectName: 'shoonya-blog',
-  organizationName: 'shoonya',
+  organizationName: 'ksarath',
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
